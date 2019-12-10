500456cb5d93850e07d95adb04346d560ec2326f
LeuAlmeida <leo@webid.net.br>
2019-12-10 16:04:32 -0300

    Improving fixed plugin

--- src/components/FixedPlugin/FixedPlugin.js
+++ src/components/FixedPlugin/FixedPlugin.js
@@ -35,46 +35,9 @@ class FixedPlugin extends Component {
             <i className="fa fa-cog fa-2x" />
           </div>
           <ul className="dropdown-menu show">
-            <li className="header-title">SIDEBAR BACKGROUND</li>
-            <li className="adjustments-line">
-              <div className="badge-colors text-center">
-                <span
-                  className={
-                    this.props.bgColor === "primary"
-                      ? "badge filter badge-primary active"
-                      : "badge filter badge-primary"
-                  }
-                  data-color="primary"
-                  onClick={() => {
-                    this.props.handleBgClick("primary");
-                  }}
-                />{" "}
-                <span
-                  className={
-                    this.props.bgColor === "blue"
-                      ? "badge filter badge-info active"
-                      : "badge filter badge-info"
-                  }
-                  data-color="blue"
-                  onClick={() => {
-                    this.props.handleBgClick("blue");
-                  }}
-                />{" "}
-                <span
-                  className={
-                    this.props.bgColor === "green"
-                      ? "badge filter badge-success active"
-                      : "badge filter badge-success"
-                  }
-                  data-color="green"
-                  onClick={() => {
-                    this.props.handleBgClick("green");
-                  }}
-                />{" "}
-              </div>
-            </li>
-            <li className="adjustments-line text-center color-change">
-              <span className="color-label">LIGHT MODE</span>{" "}
+            <li className="header-title">CONFIGURAÇÕES</li>
+            <li className="adjustments-line text-center color-change button-container">
+              <span className="color-label">MODO CLARO</span>{" "}
               <span
                 className="badge light-badge mr-2"
                 onClick={() => this.activateMode("light")}
@@ -83,38 +46,7 @@ class FixedPlugin extends Component {
                 className="badge dark-badge ml-2"
                 onClick={() => this.activateMode("dark")}
               />{" "}
-              <span className="color-label">DARK MODE</span>{" "}
-            </li>
-            <li className="button-container">
-              <Button
-                href="https://www.creative-tim.com/product/black-dashboard-react"
-                color="primary"
-                block
-                className="btn-round"
-              >
-                Download Now
-              </Button>
-              <Button
-                color="default"
-                block
-                className="btn-round"
-                outline
-                href="https://demos.creative-tim.com/black-dashboard-react/#/documentation/tutorial"
-              >
-                Documentation
-              </Button>
-            </li>
-            <li className="header-title">Want more components?</li>
-            <li className="button-container">
-              <Button
-                href="https://www.creative-tim.com/product/black-dashboard-pro-react"
-                className="btn-round"
-                disabled
-                block
-                color="danger"
-              >
-                Get pro version
-              </Button>
+              <span className="color-label">MODO ESCURO</span>{" "}
             </li>
           </ul>
         </div>
