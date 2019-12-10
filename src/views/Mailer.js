f61790c1c62f63e1eb558fe893ffeb6f5345bbc1
LeuAlmeida <leo@webid.net.br>
2019-12-10 19:49:31 -0300

    Incrementing mailer page

--- src/views/Mailer.js
+++ src/views/Mailer.js
@@ -1,5 +1,4 @@
-
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 // reactstrap components
 import {
@@ -7,14 +6,15 @@ import {
   Card,
   CardHeader,
   CardBody,
-  CardFooter,
   CardText,
+  CardFooter,
   FormGroup,
   Form,
   Input,
   Row,
-  Col
-} from "reactstrap";
+  Col,
+  TextArea,
+} from 'reactstrap';
 
 class Mailer extends Component {
   render() {
@@ -30,110 +30,85 @@ class Mailer extends Component {
                 <CardBody>
                   <Form>
                     <Row>
-                      <Col className="pr-md-1" md="5">
-                        <FormGroup>
-                          <label>Company (disabled)</label>
-                          <Input
-                            defaultValue="Creative Code Inc."
-                            disabled
-                            placeholder="Company"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="px-md-1" md="3">
-                        <FormGroup>
-                          <label>Username</label>
-                          <Input
-                            defaultValue="michael23"
-                            placeholder="Username"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="pl-md-1" md="4">
-                        <FormGroup>
-                          <label htmlFor="exampleInputEmail1">
-                            Email address
-                          </label>
-                          <Input placeholder="mike@email.com" type="email" />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
                       <Col className="pr-md-1" md="6">
                         <FormGroup>
-                          <label>First Name</label>
+                          <label>Remetente</label>
                           <Input
-                            defaultValue="Mike"
-                            placeholder="Company"
+                            defaultValue="informes@metodista.br"
+                            disabled
                             type="text"
                           />
                         </FormGroup>
                       </Col>
                       <Col className="pl-md-1" md="6">
                         <FormGroup>
-                          <label>Last Name</label>
-                          <Input
-                            defaultValue="Andrew"
-                            placeholder="Last Name"
-                            type="text"
-                          />
+                          <label>E-mail do Remetente</label>
+                          <Input type="select" name="select" id="exampleSelect">
+                            <option style={{ backgroundColor: '#d570da' }}>
+                              UMESP
+                            </option>
+                            <option style={{ backgroundColor: '#d570da' }}>
+                              UNIMEP
+                            </option>
+                          </Input>
                         </FormGroup>
                       </Col>
-                    </Row>
-                    <Row>
-                      <Col md="12">
+
+                      <Col className="pr-md-1" md="8">
                         <FormGroup>
-                          <label>Address</label>
-                          <Input
-                            defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                            placeholder="Home Address"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="4">
-                        <FormGroup>
-                          <label>City</label>
-                          <Input
-                            defaultValue="Mike"
-                            placeholder="City"
-                            type="text"
-                          />
+                          <label htmlFor="exampleInputEmail1">
+                            Assunto da Ação
+                          </label>
+                          <Input placeholder="mike@email.com" type="email" />
                         </FormGroup>
                       </Col>
                       <Col className="px-md-1" md="4">
                         <FormGroup>
-                          <label>Country</label>
+                          <label htmFor="">Autor do Disparo</label>
                           <Input
-                            defaultValue="Andrew"
-                            placeholder="Country"
                             type="text"
+                            defaultValue="Léu Almeida"
+                            disabled
                           />
                         </FormGroup>
                       </Col>
-                      <Col className="pl-md-1" md="4">
+
+                      <Col className="pr-md-1" md="6">
                         <FormGroup>
-                          <label>Postal Code</label>
-                          <Input placeholder="ZIP Code" type="number" />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col md="8">
-                        <FormGroup>
-                          <label>About Me</label>
+                          <label htmFor="">Agendar Envio</label>
                           <Input
-                            cols="80"
-                            defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                            that two seat Lambo."
-                            placeholder="Here can be your description"
-                            rows="4"
-                            type="textarea"
+                            type="datetime-local"
+                            style={{ textTransform: 'uppercase' }}
                           />
+                        </FormGroup>
+                      </Col>
+
+                      <Col className="pl-md-1" md="3">
+                        <FormGroup>
+                          <label htmlFor="exampleInputEmail1">
+                            URL da Mensagem
+                          </label>
+                          <Input placeholder="mike@email.com" type="email" />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-md-1" md="3">
+                        <FormGroup>
+                          <Button
+                            className="btn-fill mt-md-4"
+                            color="warning"
+                            type="button"
+                            style={{ width: '100%' }}
+                          >
+                            Visualizar
+                          </Button>
+                        </FormGroup>
+                      </Col>
+                      <Col className="py-md-1" md="12">
+                        <FormGroup>
+                          <label htmlFor="exampleInputEmail1">
+                            Destinatários
+                          </label>
+                          <Input placeholder="leonardo.almeida@metodista.br" type="textarea" />
                         </FormGroup>
                       </Col>
                     </Row>
@@ -146,46 +121,6 @@ class Mailer extends Component {
                 </CardFooter>
               </Card>
             </Col>
-            {/* <Col md="4">
-              <Card className="card-user">
-                <CardBody>
-                  <CardText />
-                  <div className="author">
-                    <div className="block block-one" />
-                    <div className="block block-two" />
-                    <div className="block block-three" />
-                    <div className="block block-four" />
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
-                      <img
-                        alt="..."
-                        className="avatar"
-                        src={require("assets/img/emilyz.jpg")}
-                      />
-                      <h5 className="title">Mike Andrew</h5>
-                    </a>
-                    <p className="description">Ceo/Co-Founder</p>
-                  </div>
-                  <div className="card-description">
-                    Do not be scared of the truth because we need to restart the
-                    human foundation in truth And I love you like Kanye loves
-                    Kanye I love Rick Owens’ bed design but the back is...
-                  </div>
-                </CardBody>
-                <CardFooter>
-                  <div className="button-container">
-                    <Button className="btn-icon btn-round" color="facebook">
-                      <i className="fab fa-facebook" />
-                    </Button>
-                    <Button className="btn-icon btn-round" color="twitter">
-                      <i className="fab fa-twitter" />
-                    </Button>
-                    <Button className="btn-icon btn-round" color="google">
-                      <i className="fab fa-google-plus" />
-                    </Button>
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col> */}
           </Row>
         </div>
       </>
