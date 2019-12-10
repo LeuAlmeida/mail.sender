3291573473951bc8a913c4005df92fd2cf2f394c
LeuAlmeida <leo@webid.net.br>
2019-12-10 15:25:27 -0300

    Improving routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
+// import Login from "views/Login.js";
 import Dashboard from "views/Dashboard.js";
 import Icons from "views/Icons.js";
-import Map from "views/Map.js";
 import Notifications from "views/Notifications.js";
 import TableList from "views/TableList.js";
 import Typography from "views/Typography.js";
 import UserProfile from "views/UserProfile.js";
 
 var routes = [
+  // {
+  //   path: "/login",
+  //   name: "Login",
+  //   icon: "tim-icons icon-chart-pie-36",
+  //   component: Login,
+  // },
   {
     path: "/dashboard",
     name: "Dashboard",
@@ -19,13 +25,6 @@ var routes = [
     name: "Icons",
     icon: "tim-icons icon-atom",
     component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/map",
-    name: "Map",
-    icon: "tim-icons icon-pin",
-    component: Map,
     layout: "/admin"
   },
   {
