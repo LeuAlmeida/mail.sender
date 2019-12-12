6f3b22a6b10bac8e4591b99178d0cf06a7b84fc9
LeuAlmeida <leo@webid.net.br>
2019-12-12 17:18:57 -0300

    Improving index mailer list stylesheets

---
+++ src/views/Mailer/indexStyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PaginationButton = styled.button`
+  background: none;
+  border: none;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    background: #2773f6;
+    border-radius: 50px;
+    padding: 5px;
+  }
+`;
