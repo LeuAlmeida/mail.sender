a7366b80768a62af02ac81c5f1dd3f628a7d496f
LeuAlmeida <leo@webid.net.br>
2019-12-13 18:56:39 -0300

    feat: Create get users method

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,9 @@
 export const TOKEN_KEY = '@mailsender_token';
+export const USER = '@mailsender_user';
+export const setUser = user => {
+  localStorage.setItem(USER, user);
+};
+export const getUser = () => localStorage.getItem(USER);
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = token => {
@@ -6,4 +11,5 @@ export const login = token => {
 };
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(USER);
 };
