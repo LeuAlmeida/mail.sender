ad050ca225884e0a8837d33c922eb7edc2a3b770
Léu Almeida <leo@webid.net.br>
2019-12-15 01:24:55 -0300

    tests: Testing new features

--- src/views/Tests.js
+++ src/views/Tests.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { FormGroup, Row, Col, Label, CustomInput } from 'reactstrap';
+import axios, { post } from 'axios';
 
-export default function Tests() {
-  return (
-    <Row>
-      <Col className="py-md-1" md="8">
-        <FormGroup>
-          <Label for="importRecipients">Extrair destinatários</Label>
-          <CustomInput
-            type="file"
-            id="importRecipients"
-            name="customFile"
-            className="text-primary"
-          />
-        </FormGroup>
-      </Col>
-    </Row>
-  );
+class Tests extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.fileUpload = this.fileUpload.bind(this);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault(); // Stop form submit
+    this.fileUpload(this.state.file).then(response => {
+      console.log(response.data);
+    });
+  }
+
+  onChange(e) {
+    this.setState({ file: e.target.files[0] });
+  }
+
+  fileUpload(file) {
+    const url = 'http://localhost:3333/files';
+    const formData = new FormData();
+    formData.append('file', file);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    return post(url, formData, config);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange} />
+        <button type="submit">Upload</button>
+      </form>
+    );
+  }
 }
+
+export default Tests;
