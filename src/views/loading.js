5076cda89112df841cbc4b3e508cbd1a1f998da7
LeuAlmeida <leo@webid.net.br>
2019-12-16 13:42:46 -0300

    feat: Improving loading component

---
+++ src/views/loading.js
@@ -0,0 +1,25 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  svg {
+    animation: ${rotate} 2s linear infinite;
+  }
+`;
