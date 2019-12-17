2192316b286885fb4f14f196bb6961bac4de3a12
LeuAlmeida <leo@webid.net.br>
2019-12-17 10:26:16 -0300

    feat: Import api methods to sender edit

--- src/views/Senders/Edit.js
+++ src/views/Senders/Edit.js
@@ -1,18 +1,86 @@
 import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+} from 'reactstrap';
+
+import api from '../../services/api';
 
 class EditSenders extends Component {
   state = {};
 
-  componentDidMount() {
+  async componentDidMount() {
     const { history } = this.props;
 
     const { state: id } = history.location;
 
-    console.log(id);
+    const response = await api.put(`/senders/${id}`);
+
+    console.log(response.data);
   }
 
   render() {
-    return <></>;
+    return (
+      <>
+        <ToastContainer autoClose={4500} />
+        <div className="content">
+          <Row>
+            <Col md="12">
+              <Card>
+                <CardHeader>
+                  <h5 className="title">Cadastrar Remetente</h5>
+                </CardHeader>
+                <CardBody>
+                  <Form>
+                    <Row>
+                      <Col className="pr-md-1" md="6">
+                        <FormGroup>
+                          <label>Nome do Remetente</label>
+                          <Input
+                            placeholder="Digite o nome do remetente"
+                            type="text"
+                            onChange={this.handleSetName}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pr-md-1" md="6">
+                        <FormGroup>
+                          <label>E-mail do Remetente</label>
+                          <Input
+                            placeholder="Digite o e-mail do remetente"
+                            type="text"
+                            onChange={this.handleSetEmail}
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                  </Form>
+                </CardBody>
+                <CardFooter>
+                  <Button
+                    className="btn-fill"
+                    color="primary"
+                    type="submit"
+                    onClick={this.handleSubmit}
+                  >
+                    Enviar
+                  </Button>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
   }
 }
 
