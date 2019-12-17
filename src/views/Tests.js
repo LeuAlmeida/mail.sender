b246ca0243b6814a029954f85e9e8048776c6690
LeuAlmeida <leo@webid.net.br>
2019-12-17 18:27:46 -0300

    test: Testing array populating

--- src/views/Tests.js
+++ src/views/Tests.js
@@ -7,11 +7,6 @@ export default function views() {
   const arr = data.split(',');
   console.log(arr);
 
-  let length = 5; // user defined length
-
-  for (let i = 0; i < length; i++) {
-    data.push(createSomeObject());
-  }
   return (
     <>
       <h1>Ok!</h1>
