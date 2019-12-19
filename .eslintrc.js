553ae35f13981aa76c3b80cc5f03679dca5bbc36
LeuAlmeida <leo@webid.net.br>
2019-12-19 10:16:33 -0300

    chore: Improving eslint props no spreading rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
       'warn',
       { extensions: ['.jsx', '.js'] }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off'
   },
 };
