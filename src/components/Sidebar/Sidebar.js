9ea0db2b85a26852b1971ee5539bee7a5c354ecf
LeuAlmeida <leo@webid.net.br>
2019-12-19 14:57:54 -0300

    refactor: Remove unusuless content

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -14,7 +14,6 @@ class Sidebar extends React.Component {
     super(props);
     this.activeRoute.bind(this);
   }
-  // verifies if routeName is the one active (in browser input)
   activeRoute(routeName) {
     return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
   }
@@ -131,21 +130,13 @@ Sidebar.defaultProps = {
 };
 
 Sidebar.propTypes = {
-  // if true, then instead of the routes[i].name, routes[i].rtlName will be rendered
-  // insde the links of this component
   rtlActive: PropTypes.bool,
   bgColor: PropTypes.oneOf(["primary", "blue", "green"]),
   routes: PropTypes.arrayOf(PropTypes.object),
   logo: PropTypes.shape({
-    // innerLink is for links that will direct the user within the app
-    // it will be rendered as <Link to="...">...</Link> tag
     innerLink: PropTypes.string,
-    // outterLink is for links that will direct the user outside the app
-    // it will be rendered as simple <a href="...">...</a> tag
     outterLink: PropTypes.string,
-    // the text of the logo
     text: PropTypes.node,
-    // the image src of the logo
     imgSrc: PropTypes.string
   })
 };
